137f475ae167b8342a58f782176df3dc598ee204
Pramudya <97342901+southampere28@users.noreply.github.com>
2023-09-27 13:52:44 +0700

    new commit

--- app/src/main/java/com/polije/sem3/Dashboard.java
+++ app/src/main/java/com/polije/sem3/Dashboard.java
@@ -1,19 +1,51 @@
 package com.polije.sem3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
+
+    private BottomNavigationView btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setBackground(null);
+        btnView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        btnView.setBackground(null);
+
+        btnView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.miBook:
+                        // Handle booking item click
+                        return true;
+                    case R.id.miFavs:
+                        // Handle favorites item click
+                        return true;
+                    case R.id.placeholder:
+                        // Handle placeholder item click
+                        return true;
+                    case R.id.miNotify:
+                        // Handle notification item click
+                        return true;
+                    case R.id.miProfiles:
+                        // Handle profiles item click
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        MenuItem dashboardMenuItem = btnView.getMenu().findItem(R.id.placeholder);
+        dashboardMenuItem.setEnabled(false);
+
     }
 }
