7bcf1daf0a4dd1e3ad7f922f225b19a8d4b70fcf
Pramudya <97342901+southampere28@users.noreply.github.com>
2023-11-10 16:17:49 +0700

    new update

--- app/src/main/java/com/polije/sem3/DetailInformasi.java
+++ app/src/main/java/com/polije/sem3/DetailInformasi.java
@@ -1,6 +1,8 @@
 package com.polije.sem3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
@@ -13,6 +15,7 @@ import android.location.GpsStatus;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,6 +24,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textview.MaterialTextView;
 import com.polije.sem3.databinding.ActivityDetailInformasiBinding;
 import com.polije.sem3.databinding.ActivityMapJavaBinding;
 import com.polije.sem3.model.WisataModel;
@@ -48,7 +52,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class DetailInformasi extends AppCompatActivity implements MapListener, GpsStatus.Listener{
+public class DetailInformasi extends AppCompatActivity implements MapListener, GpsStatus.Listener {
 
     private MapView mMap;
     private IMapController controller;
@@ -60,30 +64,95 @@ public class DetailInformasi extends AppCompatActivity implements MapListener, G
 
     private String idSelected;
 
+    private String destination;
+
+    private ActivityDetailInformasiBinding binding;
+
+    private MapView mapView;
+
+    private AppCompatImageButton btnBack;
+
+    private ItemizedIconOverlay<OverlayItem> itemizedIconOverlay;
+
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_informasi);
 
+        btnBack = findViewById(R.id.backButtonDetail);
+
         idSelected = getIntent().getStringExtra(ID_WISATA);
 
+        itemizedIconOverlay = new ItemizedIconOverlay<>(this, new ArrayList<>(), null);
+
+//        linkmaps = "";
+
+
+        binding = ActivityDetailInformasiBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Configuration.getInstance().load(
+                getApplicationContext(),
+                getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
+        );
+
         // kodingan retrofit get data
-
-
-//        Client.getInstance().method_get_data()
         Client.getInstance().detailwisata(idSelected).enqueue(new Callback<DetailWisataResponse>() {
-            private TextView txtTitle = (TextView) findViewById(R.id.namaWisata);
-            private String namaWisata;
 
             @Override
             public void onResponse(Call<DetailWisataResponse> call, Response<DetailWisataResponse> response) {
                 if (response.body() != null && response.body().getStatus().equalsIgnoreCase("success")) {
                     dataListWisata = response.body().getData();
+                    String coordinates = dataListWisata.getCoordinate();
+                    String linkmaps = dataListWisata.getLinkmaps();
 
-                    txtTitle.setText(dataListWisata.getNama().toString());
+                    if (!coordinates.isEmpty()){
+                        // Split the string by commas
+                        String[] words = coordinates.split(",");
+                        double firstCoordinates = Double.parseDouble(words[0].trim());  // Trim to remove leading and trailing whitespaces
+                        double secondCoordinates = Double.parseDouble(words[1].trim());
 
-                    Toast.makeText(DetailInformasi.this, dataListWisata.getNama(), Toast.LENGTH_SHORT).show();
+                        mMap = binding.osmmap;
+                        mMap.setTileSource(TileSourceFactory.MAPNIK);
+                        mMap.getMapCenter();
+                        mMap.setMultiTouchControls(true);
+                        mMap.getLocalVisibleRect(new Rect());
+
+
+                        GeoPoint startPoint = new GeoPoint(firstCoordinates, secondCoordinates);
+                        OverlayItem overlayItem = new OverlayItem("Marker Title", "Marker Description", startPoint);
+                        Drawable marker = getResources().getDrawable(R.drawable.locationpin); // Ganti dengan gambar marker Anda
+                        marker.setBounds(0, 0, 10, 10);
+                        overlayItem.setMarker(marker);
+
+                        controller = mMap.getController();
+
+                        controller.setCenter(startPoint);
+                        controller.animateTo(startPoint);
+                        controller.setZoom(16);
+
+                        Log.e("TAG", "onCreate:in " + controller.zoomIn());
+                        Log.e("TAG", "onCreate: out " + controller.zoomOut());
+
+                        itemizedIconOverlay.addItem(overlayItem);
+                        mMap.getOverlays().add(itemizedIconOverlay);
+                        mMap.addMapListener(DetailInformasi.this);
+                    }
+
+                    if (linkmaps.isEmpty()) {
+                        Toast.makeText(DetailInformasi.this, "Lokasi tidak tersedia", Toast.LENGTH_SHORT).show();
+                        destination = "Air+Terjun+Sedudo";
+                    } else if (!linkmaps.isEmpty()) {
+                        destination = linkmaps;
+                    }
+
+                    binding.namaWisata.setText(dataListWisata.getNama());
+                    binding.deskripsiWisata.setText(dataListWisata.getDeskripsi());
+                    binding.jamOperasional.setText(dataListWisata.getJadwal());
+                    binding.hargaTiket.setText(dataListWisata.getHarga_tiket());
+                    binding.alamatWisata.setText(dataListWisata.getAlamat());
+
+                    Toast.makeText(DetailInformasi.this, coordinates, Toast.LENGTH_SHORT).show();
 
                 }
             }
@@ -96,39 +165,8 @@ public class DetailInformasi extends AppCompatActivity implements MapListener, G
             }
         });
 
-        ActivityDetailInformasiBinding binding = ActivityDetailInformasiBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        Configuration.getInstance().load(
-                getApplicationContext(),
-                getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
-        );
-        mMap = binding.osmmap;
-        mMap.setTileSource(TileSourceFactory.MAPNIK);
-        mMap.getMapCenter();
-        mMap.setMultiTouchControls(true);
-        mMap.getLocalVisibleRect(new Rect());
 
-        ItemizedIconOverlay<OverlayItem> itemizedIconOverlay = new ItemizedIconOverlay<>(this, new ArrayList<>(), null);
-        GeoPoint startPoint = new GeoPoint(-7.78142419909586, 111.75760589038738);
-        OverlayItem overlayItem = new OverlayItem("Marker Title", "Marker Description", startPoint);
-        Drawable marker = getResources().getDrawable(R.drawable.locationpin); // Ganti dengan gambar marker Anda
-        marker.setBounds(0, 0, 10, 10);
-        overlayItem.setMarker(marker);
-
-        controller = mMap.getController();
-
-        controller.setCenter(startPoint);
-        controller.animateTo(startPoint);
-        controller.setZoom(16);
-
-        Log.e("TAG", "onCreate:in " + controller.zoomIn());
-        Log.e("TAG", "onCreate: out " + controller.zoomOut());
-
-        itemizedIconOverlay.addItem(overlayItem);
-        mMap.getOverlays().add(itemizedIconOverlay);
-        mMap.addMapListener(this);
-
-        MapView mapView = findViewById(R.id.osmmap);
+        mapView = findViewById(R.id.osmmap);
         ScrollView scrollView = (ScrollView) findViewById(R.id.scrollviewLayout);
 
         // smooth scroll map
@@ -155,7 +193,7 @@ public class DetailInformasi extends AppCompatActivity implements MapListener, G
         btnLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String destination = "Air+Terjun+Sedudo"; // Gantilah dengan nama atau alamat tujuan Anda
+//                destination = "Air+Terjun+Sedudo"; // Gantilah dengan nama atau alamat tujuan Anda
                 String mapUri = "https://www.google.com/maps/search/?api=1&query=" + destination;
 //                String mapUri = "https://maps.app.goo.gl/" + destination;
 
@@ -180,26 +218,33 @@ public class DetailInformasi extends AppCompatActivity implements MapListener, G
             }
         });
 
-    }
-
-        @Override
-        public boolean onScroll(ScrollEvent event) {
-            Log.e("TAG", "onCreate:la " + event.getSource().getMapCenter().getLatitude());
-            Log.e("TAG", "onCreate:lo " + event.getSource().getMapCenter().getLongitude());
-            return true;
-        }
-
-        @Override
-        public boolean onZoom(ZoomEvent event) {
-            Log.e("TAG", "onZoom zoom level: " + event.getZoomLevel() + "   source:  " + event.getSource());
-            return false;
-        }
-
-        @Override
-        public void onGpsStatusChanged(int event) {
-            // Handle GPS status changes
-        }
-
-
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailInformasi.this, ListWisata.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
+    @Override
+    public boolean onScroll(ScrollEvent event) {
+        Log.e("TAG", "onCreate:la " + event.getSource().getMapCenter().getLatitude());
+        Log.e("TAG", "onCreate:lo " + event.getSource().getMapCenter().getLongitude());
+        return true;
+    }
+
+    @Override
+    public boolean onZoom(ZoomEvent event) {
+        Log.e("TAG", "onZoom zoom level: " + event.getZoomLevel() + "   source:  " + event.getSource());
+        return false;
+    }
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+        // Handle GPS status changes
+    }
+
+
+}
